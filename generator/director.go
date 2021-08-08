95ea8ba41f1e7f662686e02fe8b21d00bc39f2e0
agocan <578843228@qq.com>
2021-08-08 22:56:58 +0800

    可以执行，先放着

--- generator/director.go
+++ generator/director.go
@@ -1,3 +1,28 @@
 package generator
 
-// 目录生成器
+// 创建目录需要单独运行，不放在GeneratorMgr里面
+import (
+	"os"
+	"path"
+)
+
+var dirs = []string{
+	"styles",
+	"assets",
+}
+
+// DirGenerator 目录生成器
+type DirGenerator struct {
+}
+
+// Run 运行生成器
+func (d *DirGenerator) Run(opt *Option) (err error) {
+	for _, dir := range dirs {
+		fullDir := path.Join(opt.AbsProjectPath, dir)
+		err = os.MkdirAll(fullDir, 0755)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	_ "embed"
+	"path"
 
+	"github.com/agocan/gitbook-generator/config"
+	"github.com/agocan/gitbook-generator/generator"
+)
+
+var Option generator.Option
+
+var (
+	//go:embed template/book.json.tmpl
+	apiContent string
+)
+
+var files = map[string]string{
+	"book.json": apiContent,
+}
+
+func main() {
+	config.DefaultConfig()
+	generator.Init()
+
+	Option = generator.Option{
+		AbsProjectPath: config.AbsProjectPath,
+		Plugins:        config.Plugins,
+		Title:          *config.Title,
+		SidebarLink:    *config.SidebarLink,
+		SidebarTitle:   *config.SidebarTitle,
+		Author:         *config.Author,
+		Description:    *config.Description,
+		Keywords:       *config.Keywords,
+		GaToken:        *config.GaToken,
+		TreeBaToken:    *config.TreeBaToken,
+	}
+	// Option.AbsProjectPath = path.Join(*config.ProjectPath, *config.Title)
+	Option.AbsProjectPath = path.Join(*config.ProjectPath, "")
+	var fileGen generator.FileGenerator
+	fileGen.Files = files
+	// 注册
+	generator.Register("files", &fileGen)
+	generator.RunGenerator(&Option)
 }
